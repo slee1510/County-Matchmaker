d420e1b705c3d24d17281f607d19a8bb16bcdee5
sydney <sydneylee1510@gmail.com>
2025-10-28 12:04:56 -0400

    Started a basic rough draft of front end pages, mainly home page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from app import app
-from flask import render_template, request, jsonify
+from flask import render_template, request, jsonify, Blueprint
+
+bp = Blueprint('main', __name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/filter')
+def filter():
+    return render_template('filter.html')
